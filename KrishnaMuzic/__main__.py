c13c328e457cbcfd771fda8dfefccf4ee66ff939
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 19:37:18 +0530

    Update __main__.py

--- KrishnaMuzic/__main__.py
+++ KrishnaMuzic/__main__.py
@@ -51,7 +51,7 @@ async def init():
         await KRISHNA.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("KrishnaMuzic").error(
-            'Please turn on the videochat of your log group\channel.\n\nStopping Bot...'
+            "Please turn on the videochat of your log group\\channel.\n\nStopping Bot..."
         )
         exit()
     except:
@@ -68,6 +68,7 @@ async def init():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+
 
 
 

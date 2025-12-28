b859413556dde54b3d137e9a6a9fba22afebbb6d
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 12:29:18 +0530

    Update dev.py

--- KrishnaMuzic/plugins/tools/dev.py
+++ KrishnaMuzic/plugins/tools/dev.py
@@ -10,7 +10,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from IstkharMusic import app
+from KrishnaMuzic import app
 from config import OWNER_ID
 
 
@@ -209,3 +209,4 @@ async def shellrunner(_, message: Message):
     else:
         await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
     await message.stop_propagation()
+

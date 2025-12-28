2c7c0aa85271a8e1ce9aadea865b2603bb70fe75
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 12:10:42 +0530

    Update help.py

--- KrishnaMuzic/utils/inline/help.py
+++ KrishnaMuzic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from IstkharMusic import app
+from KrishnaMuzic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
@@ -116,3 +116,4 @@ def private_help_panel(_):
         ],
     ]
     return buttons
+

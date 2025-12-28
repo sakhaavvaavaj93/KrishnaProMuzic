39c1c5fb59c15934ddcb76485377119fca851073
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 13:11:24 +0530

    Update pause.py

--- KrishnaMuzic/plugins/admins/pause.py
+++ KrishnaMuzic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from IstkharMusic import app
-from IstkharMusic.core.call import ISTKHAR
-from IstkharMusic.utils.database import is_music_playing, music_off
-from IstkharMusic.utils.decorators import AdminRightsCheck
-from IstkharMusic.utils.inline import close_markup
+from KrishnaMuzic import app
+from KrishnaMuzic.core.call import KRISHNA
+from KrishnaMuzic.utils.database import is_music_playing, music_off
+from KrishnaMuzic.utils.decorators import AdminRightsCheck
+from KrishnaMuzic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,10 +16,11 @@ async def pause_admin(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
     try:
-        await ISTKHAR.pause_stream(chat_id)
+        await KRISHNA.pause_stream(chat_id)
     except Exception:
         pass
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention),
         reply_markup=close_markup(_)
+
     )

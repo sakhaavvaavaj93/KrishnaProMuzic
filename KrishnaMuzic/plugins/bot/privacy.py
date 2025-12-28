c9393fcbda84d49d7c91b01f4ef12e49856e220b
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 13:00:01 +0530

    Update privacy.py

--- KrishnaMuzic/plugins/bot/privacy.py
+++ KrishnaMuzic/plugins/bot/privacy.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pyrogram.enums import ParseMode
-from IstkharMusic import app
+from KrishnaMuzic import app
 import config
 
 TEXT = f"""

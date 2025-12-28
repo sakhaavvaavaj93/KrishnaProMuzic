ea2d57409df7c6240e7f694fc975925e69d3c257
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 19:06:59 +0530

    Update logger.py

--- KrishnaMuzic/plugins/sudo/logger.py
+++ KrishnaMuzic/plugins/sudo/logger.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from KrishnaMuzic import app
 from KrishnaMuzic.misc import SUDOERS
 from KrishnaMuzic.utils.database import add_off, add_on
-from KrishnaMusic.utils.decorators.language import language
+from KrishnaMuzic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
@@ -27,5 +27,6 @@ async def logger(client, message, _):
 async def logger(client, message, _):
     await message.reply_document("cookies/logs.csv")
     await message.reply_text("Please check given file to cookies file choosing logs...")
+
 
 

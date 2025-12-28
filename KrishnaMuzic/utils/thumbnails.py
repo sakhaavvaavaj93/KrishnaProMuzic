e1f3e101b90d2c635bcc2622bdb30e68fd1fe109
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 11:32:05 +0530

    Update thumbnails.py

--- KrishnaMuzic/utils/thumbnails.py
+++ KrishnaMuzic/utils/thumbnails.py
@@ -181,9 +181,9 @@ async def gen_thumb(videoid: str):
         background = Image.blend(background, gradient_image, alpha=0.2)
         
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("IstkharMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("IstkharMusic/assets/font.ttf", 30)
-        title_font = ImageFont.truetype("IstkharMusic/assets/font3.ttf", 45)
+        arial = ImageFont.truetype("KrishnaMuzic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("KrishnaMuzic/assets/font.ttf", 30)
+        title_font = ImageFont.truetype("KrishnaMuzic/assets/font3.ttf", 45)
 
 
         circle_thumbnail = crop_center_circle(youtube, 400, 20, start_gradient_color)
@@ -233,7 +233,7 @@ async def gen_thumb(videoid: str):
         draw_text_with_shadow(background, draw, (text_x_position, 400), "00:00", arial, (255, 255, 255))
         draw_text_with_shadow(background, draw, (1080, 400), duration, arial, (255, 255, 255))
         
-        play_icons = Image.open("IstkharMusic/assets/play_icons.png")
+        play_icons = Image.open("KrishnaMuzic/assets/play_icons.png")
         play_icons = play_icons.resize((580, 62))
         background.paste(play_icons, (text_x_position, 450), play_icons)
 
@@ -248,3 +248,4 @@ async def gen_thumb(videoid: str):
         logging.error(f"Error generating thumbnail for video {videoid}: {e}")
         traceback.print_exc()
         return None
+

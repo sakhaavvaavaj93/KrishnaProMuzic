acd340a8174cbb086a90f3c027e77275365c23a8
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 13:02:12 +0530

    Update stop.py

--- KrishnaMuzic/plugins/admins/stop.py
+++ KrishnaMuzic/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from IstkharMusic import app
-from IstkharMusic.core.call import ISTKHAR
-from IstkharMusic.utils.database import set_loop
-from IstkharMusic.utils.decorators import AdminRightsCheck
-from IstkharMusic.utils.inline import close_markup
+from KrishnaMuzic import app
+from KrishnaMuzic.core.call import ISTKHAR
+from KrishnaMuzic.utils.database import set_loop
+from KrishnaMuzic.utils.decorators import AdminRightsCheck
+from KrishnaMuzic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -16,9 +16,10 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if len(message.command) != 1:
         return
-    await ISTKHAR.stop_stream(chat_id)
+    await KRISHNA.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention),
         reply_markup=close_markup(_)
     )
+

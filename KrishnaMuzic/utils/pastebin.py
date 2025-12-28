77d48afd70777747742277f4587c060bf87b515d
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 11:33:51 +0530

    Update pastebin.py

--- KrishnaMuzic/utils/pastebin.py
+++ KrishnaMuzic/utils/pastebin.py
@@ -13,9 +13,10 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def ISTKHARBin(text):
+async def KRISHNABin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
     link = BASE + resp["message"]
     return link
+

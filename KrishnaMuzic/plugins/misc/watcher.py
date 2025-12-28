e72d79486b4b8947d5bce7af087c10aa27366107
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 15:40:45 +0530

    Update watcher.py

--- KrishnaMuzic/plugins/misc/watcher.py
+++ KrishnaMuzic/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from KrishnaMuzic import app
-from KrishnaMuzic.core.call import KRISHNA
+from KrishnaMuzic.core.bot import KRISHNA
 
 welcome = 20
 close = 30
@@ -12,4 +12,5 @@ close = 30
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
     await ISTKHAR.stop_stream_force(message.chat.id)
+
 

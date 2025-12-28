112fded594402a7c0c29747a4fb5b0471fd02f1b
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2025-12-28 15:38:15 +0530

    Update reload.py

--- KrishnaMuzic/plugins/tools/reload.py
+++ KrishnaMuzic/plugins/tools/reload.py
@@ -6,7 +6,7 @@ from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
 from KrishnaMuzic import app
-from KrishnaMuzic.core.call import KRISHNA
+from KrishnaMuzic.core.bot import KRISHNA
 from KrishnaMuzic.misc import db
 from KrishnaMuzic.utils.database import get_assistant, get_authuser_names, get_cmode
 from KrishnaMuzic.utils.decorators import ActualAdminCB, AdminActual, language
@@ -121,4 +121,5 @@ async def stop_download(client, CallbackQuery: CallbackQuery, _):
         except:
             return await CallbackQuery.answer(_["tg_8"], show_alert=True)
     await CallbackQuery.answer(_["tg_9"], show_alert=True)
+
 
